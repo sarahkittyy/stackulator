e9d28ab92858b36d24ad593a34fca80114a377d9
Sarah Ohlin <sarahohlin1@gmail.com>
2019-10-08 22:15:07 -0400

    added show instance for Evaluate

--- src/Values.hs
+++ src/Values.hs
@@ -2,9 +2,20 @@
 module Values where
 
 -- | A value that can be stored in the stack
-data Value = Operator Char | Number Float
+data Value = Operator Char | Number Float | Evaluate
 
 -- | For showing those values.
 instance Show Value where
     show (Operator x) = "x"
     show (Number x) = show x
+    show Evaluate = "eval"
+    
+-- | Converts a string to a value appendable to the stack.
+fromInput :: String -> Value
+fromInput x
+    | x == "+" = Operator '+'
+    | x == "-" = Operator '-'
+    | x == "*" = Operator '*'
+    | x == "/" = Operator '/'
+    | x == "eval" = Evaluate
+fromInput x = Number (read x :: Float)
