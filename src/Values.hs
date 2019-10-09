728b0ff5fe301bdf407606dad83a2f8b519ce751
Sarah Ohlin <sarahohlin1@gmail.com>
2019-10-08 22:30:34 -0400

    added values to read from stdin / add to stack

--- src/Values.hs
+++ src/Values.hs
@@ -1,21 +1,29 @@
 -- | Defines all the values that are stored in the stackulator's stack.
 module Values where
+    
+import Text.Read ( readMaybe )
 
 -- | A value that can be stored in the stack
-data Value = Operator Char | Number Float | Evaluate
+data Value = Operator Char | Number Float | Evaluate | Pop
 
 -- | For showing those values.
 instance Show Value where
-    show (Operator x) = "x"
+    show (Operator x) = [x]
     show (Number x) = show x
     show Evaluate = "eval"
     
 -- | Converts a string to a value appendable to the stack.
-fromInput :: String -> Value
+fromInput :: String -> Maybe Value
 fromInput x
-    | x == "+" = Operator '+'
-    | x == "-" = Operator '-'
-    | x == "*" = Operator '*'
-    | x == "/" = Operator '/'
-    | x == "eval" = Evaluate
-fromInput x = Number (read x :: Float)
+    | x == "+" = Just $ Operator '+'
+    | x == "-" = Just $ Operator '-'
+    | x == "*" = Just $ Operator '*'
+    | x == "/" = Just $ Operator '/'
+    | x == "eval" = Just Evaluate
+    | x == "pop" = Just Pop
+fromInput x =
+    let num = readMaybe x :: Maybe Float
+    in
+        case num of
+            Just x -> Just (Number x)
+            Nothing -> Nothing
